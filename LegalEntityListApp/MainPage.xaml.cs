c6576bae02d4b50750ac7be25bb420a84cdb49a7
delchew <delchew@mail.ru>
2021-07-08 00:06:21 +0300

    Added data loading and main page

---
+++ LegalEntityListApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LegalEntityListApp.ViewModels;
+using Xamarin.Forms;
+
+namespace LegalEntityListApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly LegalEntityListViewModel _legalEntityList;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _legalEntityList = new LegalEntityListViewModel();
+            BindingContext = _legalEntityList;
+        }
+
+        private async void MapButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MapPage());
+        }
+    }
+}
